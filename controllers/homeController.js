ee16f85ddd1c2d0a76a099592af3260ad424d74f
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2022-12-04 21:01:51 -0300

    P20

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,8 +3,8 @@ const {Router} = require('express');
 const roteador = Router();
 
 roteador.get('/', async(req, res)=>{
-
-    res.status(200).render('usuario/home');
+    logado = req.session.login
+    res.status(200).render('usuario/home', {logado});
 });
 
 module.exports = roteador;
