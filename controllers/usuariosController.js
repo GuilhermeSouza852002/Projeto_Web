f94262ae15b84397744ce7522103eb80a1c9baee
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2022-12-05 02:13:24 -0300

    P22

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -6,6 +6,19 @@ const roteador = Router();
 //CRUD - Create, Read, Update, Destroy
 //em REST
 
+
+//configurações do usuario
+roteador.get('/settings', async(req, res)=>{
+    const usuarios = await User.findAll({
+
+     });
+    usuarios.username = usuarios.User?.username;
+    res.status(200).render('usuario/settings', {usuarios});
+});
+
+
+
+//Criação de usuario
 roteador.post('/novo', async (req, res)=>{
     const {username, useremail, password} = req.body;
     await User.create({username, useremail, password});
@@ -15,27 +28,32 @@ roteador.post('/novo', async (req, res)=>{
        
 });
 
+//get tela de login
 roteador.get('/login', async(req, res)=>{
     res.status(200).render('usuario/login');
 });
 
+//get tela de logoff
 roteador.get('/logoff', async(req, res)=>{
     req.session.destroy();
     res.redirect('/home');
 });
 
+//get tela do formulario
 roteador.get('/cadastrousuario', (req, res)=>{
     res.status(200).render('usuario/formularioCadastroUsuario');    
 });
 
+//lista usuario
 roteador.get('/:id', async(req, res)=>{
-    //res.status(200).render('apresenta', {usuario});
 });
 
+//edita usuario
 roteador.get('/:id/edit', async(req, res)=>{
 
 });
 
+//fazer login com email e senha
 roteador.post('/login', async (req, res)=>{
     const {useremail, password} = req.body;
     
@@ -46,17 +64,18 @@ roteador.post('/login', async (req, res)=>{
         }   
     });
 
+//criando sessão ao validar login
     req.session.login = false;
     if(user){
         req.session.login = true;
-        res.redirect('/publicacoes');
+        res.redirect('/home');
     }else{
         res.redirect('/usuarios/login');
     }   
        
 });
 
-
+//apaga usuario
 roteador.delete('/:id', async(req, res)=>{
 
 });
